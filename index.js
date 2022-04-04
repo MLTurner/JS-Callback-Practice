2102f33004c6692570fd208f7a738763dc5e51bc
MLTurner <mlturne3@ncsu.edu>
2022-04-03 20:51:32 -0400

    made character walk, refactored using callbacks

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
+let direction = null;
+let x = 100;
+let y = 250;
 move(character).to(100, 250)
 
 
@@ -13,3 +16,139 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+// moveCharacter function
+
+/*function moveCharacter(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}
+
+// setInterval
+
+setInterval(moveCharacter, 1)                                    
+
+
+// addEventListener to have the element move when user presses arrow keys
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+})
+
+
+// make sure element stops when user is not pressing an arrow key
+
+document.addEventListener('keyup', function(e){
+    direction = null
+})
+*/
+
+// Refactoring the code to make it simpler
+
+function move(element) {
+    element.style.position = 'fixed'
+
+    function moveToCoordinates(left, bottom) {
+        element.style.left = left + 'px'
+        element.style.bottom = bottom + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+       let direction = null;
+       let x = left;
+       let y= bottom;
+
+       element.style.left = x + 'px'
+       element.style.bottom = y + 'px'
+
+       function moveCharacter(){ 
+        if(direction === 'west'){
+            x = x - 1
+        }
+        if(direction === 'north'){
+            y = y + 1
+        }
+        if(direction === 'east'){
+            x = x + 1
+        }
+        if(direction === 'south'){
+            y = y - 1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+    setInterval(moveCharacter, 1)  
+
+    document.addEventListener('keydown', function(e){
+        if(e.repeat) return;
+    
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+        callback(direction)
+    })
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
+
+}
+
+    return {
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
+    }        
+}
+function handleDirectionChange(direction){
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
+}
+move(character).withArrowKeys(100, 250, handleDirectionChange)
+
